2a9ecab5231e7a4cdca6124028c48eed0e79bde5
Babouchot <bastien.maureille@hotmail.fr>
2013-12-14 15:01:03 +0100

    yikes

--- Source/Child.hx
+++ Source/Child.hx
@@ -1,16 +1,39 @@
 package;
 
+import flash.display.Stage;
+import flash.events.KeyboardEvent;
+import flash.ui.Keyboard;
+
 class Child extends Entity {
 
-    private static var counter:Int;
-	
-	public function new (xPositon:Int, yPosition:Int) {
+    private var lane(default, set):Int; // Lane number the character is in
+	public var speed:Int;
+
+	public function new (stage:Stage,lane:Int) {
 		
-		super (xPositon, yPosition, "assets/child.png");
+		super(stage.stageWidth, stage.stageHeight - 50, "assets/child.png",stage);
+		Skin.scaleX = 0.3;
+		Skin.scaleY = 0.3;
+		this.lane = lane;
+		speed = 10;
+	}		
+
 				
+	function set_lane(value:Int):Int {
+		switch(value) {
+			case 0:
+				YPosition = 300;
+			case 1:
+				YPosition = 200;
+			case 2 :
+				YPosition = 150;
+
+		}
+		//YPosition = value * 100 + 50;
+		
+		return lane=value;
 	}
-
-
+	
 
 	public override function onCollision () : Void {
 		super.onCollision();
@@ -20,6 +43,7 @@ class Child extends Entity {
 	public override function update (deltaTime:Int) : Void {
 		super.update(deltaTime);
 		// TODO 
+		XPosition -= speed;
 	}
 	
 }
