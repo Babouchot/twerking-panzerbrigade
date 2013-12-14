6a6379389cd3d9008c34356076ee15237e68f7b6
ElieJacquelin <elie.jacquelin@free.fr>
2013-12-15 00:13:33 +0100

    Fixed flahs and added destroying children

--- Source/Entity.hx
+++ Source/Entity.hx
@@ -10,9 +10,15 @@ import openfl.Assets;
 class Entity extends MyAnimation {
 
 	private var XPosition(default,set):Int;
-	private var YPosition(default,set):Int;
+	private var YPosition(default, set):Int;
+	private var radius:Float;
+	public var lane(default,set):Int;
 	// private var stage:Stage;
 
+	function set_lane(value:Int):Int {
+		return 0;
+	}
+	
 	function set_XPosition(value: Int):Int {
 		x = value;
 		return XPosition=value;
@@ -37,12 +43,36 @@ class Entity extends MyAnimation {
 	 * Function called when the entity collide another one
 	 */
 
-	public function onCollision () : Void{
+	public function onCollision (){
 
 
 	}
 	
 	public function onPress(event:KeyboardEvent) {
 		
+	}
+	
+	public inline function WhipOverlaps(lane:Int) {
+		if(this.lane==lane){
+			var whipX = 300;
+			if (XPosition - radius < whipX && XPosition + radius > whipX) {
+				onCollision();
+				return true;
+			};
+		}
+		return false;
+	}
+	
+	//collision between 2 entities
+	public inline function overlaps(e:Entity) {
+	  var maxDist = radius + e.radius;
+	  // classic distance formula
+	  var distSqr = (e.x-x)*(e.x-x) + (e.y-y)*(e.y-y);
+	  if( distSqr<=maxDist*maxDist )
+	   // square root computed here for performances sake
+	   if ( Math.sqrt(distSqr) <= maxDist) {
+		   onCollision();
+	   }
+
 	}
 }
