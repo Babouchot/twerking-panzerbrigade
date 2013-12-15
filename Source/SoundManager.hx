cf0e61495541eaacac4ad1778876897971e41960
Babouchot <bastien.maureille@hotmail.fr>
2013-12-15 21:05:21 +0100

    no more shitty wav

--- Source/SoundManager.hx
+++ Source/SoundManager.hx
@@ -46,11 +46,7 @@ class SoundManager {
 		lutins = new Sound(new URLRequest("assets/sound/lutins.wav"));
 
 
-		musicsArray = [new Sound (new URLRequest("assets/music/jingle_bells.wav")),
-						new Sound (new URLRequest("assets/music/carol_of_the_bells.wav")),
-						new Sound (new URLRequest("assets/music/Evil-Nutcracker.wav")),
-						new Sound (new URLRequest("assets/music/nightmare_bc.wav")),
-						new Sound (new URLRequest("assets/music/Nutcracker1.wav"))];
+		musicsArray = [new Sound (new URLRequest("assets/music/nightmare_bc.wav"))];
 
 		// musicsMap = ["jingle_bells" => "assets/music/jingle_bells.ogg",
 		// 										"carol" => "assets/music/carol_of_the_bells.ogg",
